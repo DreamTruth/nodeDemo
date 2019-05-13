4967a4981e5ea2a5a14dff8272d2a1bb220a8aa4
DreamTruth <dtgaowang@163.com>
2019-05-13 22:45:40 +0800

    update

---
+++ week_1/main.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer((req,res)=>{
+    var pathname = url.parse(req.url).pathname;
+    var extname = path.extname(pathname);
+    if(pathname === '/'){
+        fs.readFile('./index.html',(err,data)=>{
+            res.writeHead(200,{'Content-type':'text/html;charset=UTF-8'});
+            res.end(data);
+        })
+        return;
+    }
+    fs.readFile('./'+pathname,(err,data)=>{
+        if(err){
+            fs.readFile('./view/error.html',(err,data)=>{
+                res.writeHead(200,{'Content-type':'text/html;charset=UTF-8'});
+                res.end(data);
+            })
+            return;
+        }
+        getMime(extname,(mime)=>{
+            res.writeHead(200,{'Content-type':mime+';charset=UTF-8'});
+            res.end(data);
+        });
+    })
+})
+
+server.listen(3000,'192.168.1.143');
+
+//获取文件的 MIME 类型
+function getMime(extname,cb){
+    //读取mime.json文件
+    fs.readFile('./mime.json',(err,data)=>{
+        if(err) throw err;
+        //转成 JSON
+        var mimeJson = JSON.parse(data);
+        var mime = mimeJson[extname] || 'text/html';
+        //执行回调函数
+        cb(mime);
+    })
+}
